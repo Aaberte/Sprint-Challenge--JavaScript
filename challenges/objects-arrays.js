9f91ab6dbaca20393972aa50222193e9870ee2e4
Aaberte <aaron-bertelsen@yahoo.com>
2019-12-20 11:22:12 -0800

    answered questions on function file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,9 +72,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-  graduates.university.push(universities);
-  universities.sort();
+const universities = []
+  graduates.find(graduates.length === 3);
+
+
+
 
 console.log(universities);
 
